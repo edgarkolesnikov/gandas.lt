6947855871a898ee3f0301dba850619984af6a45
edgarkolesnikov <edgarkolesnikow@gmail.com>
2022-03-29 20:52:03 +0300

    launcer router index

--- pub/index.php
+++ pub/index.php
@@ -1,2 +1,10 @@
-<?php   // musu entry pointas i sistema
+<?php // musu entry pointas i sistema
 
+include "../config.php";
+include "../vendor/autoload.php";
+
+$router = new \Core\Router();
+$launcher = new Core\Launcher();
+
+
+$launcher-> start($router->getRouteInfo());
